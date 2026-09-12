1d5101a18e41b9113b428a4bc894f322bb48c78f
doaa91219-jpg <doaa91219@gmail.com>
2026-09-12 04:07:05 +0300

    Update companies.route.js

--- Backend/routes/companies.route.js
+++ Backend/routes/companies.route.js
@@ -1,12 +1,15 @@
-const express = require("express");
+ const express = require("express");
 const companyRouter = express.Router();
-const { getCompanies, addCompany } = require("../controller/companies.controller.js");
+const { 
+  getCompanies, 
+  addCompany, 
+  updateCompany, 
+  deleteCompany 
+} = require("../controller/companies.controller.js");
 
-// -- CRUD OPERATION -- >> get, post
 companyRouter.get("/", getCompanies);
 companyRouter.post("/", addCompany);
-
-// companyRouter.put("/:id", updateCompany);
-// companyRouter.delete("/:id", deleteCompany);
+companyRouter.put("/:id", updateCompany);
+companyRouter.delete("/:id", deleteCompany);
 
 module.exports = { companyRouter };
